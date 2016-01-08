2c1bdadbc5a03f21cfee826513ada536f38c1814
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-01-07 20:27:09 -0500

    Probando animate

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,11 @@
 $(function() {
-  $('#menuSup').slideDown(900);
-  $('#menuInf').show(800);
+  //$('#menuSup').slideDown(900);
+  //$('#menuInf').show(800);
+  $('#menuSup').animate({
+    marginTop:' 0'
+  }, 950);
+  
+  $('#menuInf').animate({
+    marginTop:' 0'
+  }, 950);
 });
